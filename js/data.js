30099903a2a679a4d20c302718afead3e79b5ee3
Luigi9411 <luigi.sciavicco@gmail.com>
2022-11-11 23:26:26 +0100

    add-commit-final

--- js/data.js
+++ js/data.js
@@ -1,3 +1,52 @@
+function renderIcons(arrIcons,eleBox){
+	eleBox.innerHTML = ''
+	arrIcons.forEach(objDate => eleBox.innerHTML += generateBox(objDate));
+
+}
+
+
+function generateBox(obj){
+    return `
+	   <div class= "box">
+	        <i class="${obj.prefix}sharp ${obj.prefix}solid ${obj.prefix}${obj.name}" style="color:${obj.color}" ></i>
+			<h4>${obj.name}</h4>
+	   </div>	
+	`
+}
+
+function populateSelect(arrIcon, eleSelectLevel){
+	const arrType = [];
+	arrIcon.forEach(objDate => arrType.includes(objDate.type) ? '': arrType.push(objDate.type));
+	arrType.forEach(type => eleSelectLevel.innerHTML += `<option value="${type}">${type}</option>`);
+}
+
+function filterIcons() {
+	const selectedType = this.value; 
+	if (selectedType !== '') {
+		arrDateFiltered = arrDate.filter(objDate => objDate.type === selectedType)
+	} else {
+		arrDateFiltered = arrDate;
+	}
+	renderIcons(arrDateFiltered, eleBoxFlex);
+}
+
+function randomizeColors(arrIcons) {
+	arrIcons.forEach(objDate => objDate.color = getRandomColor());
+}
+
+function getRandomColor() {
+	const acceptableChars = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+		color += acceptableChars[getRandomInteger(0, 15)];
+	}
+	return color; 
+}
+
+function getRandomInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 const arrDate =[
 	{
 		name: 'cat',
@@ -114,38 +163,10 @@ const arrDate =[
 ];
 
 
-// Milestone 2
-// Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
-// Milestone 3
-// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-// BONUS
-// 1- modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
-// 2- popolare le options della select della milestone 3 dinamicamente.
-// Consigli del giorno
-// Come sempre, iniziamo prima di tutto dall'analisi e comprensione della consegna. Scomponiamo il problema in micro-passaggi logici che solamente in un secondo momento trasformeremo in codice.
-// Le icone presenti nella struttura dati fanno riferimento alla nota libreria Font Awesome, perciò come prima cosa assicuriamoci di aver inserito il link alla cdn nell'head della pagina.
-// Dopodiché, basandoci sul codice di riferimento nel sito di Font Awesome, analizziamo come è formato il tag <i> di un'icona qualsiasi, in particolare focalizziamoci sulle classi.
-// Come possiamo usare i dati presenti nella nostra struttura dati per creare l'elemento html nel modo corretto e visualizzare l'icona in pagina?
-// Inizialmente può essere sufficiente stampare dei semplici div, senza alcuno stile, con all'interno l'icona e uno span con il nome. Solamente quando la parte logica è completa, ci dedichiamo al css.
-
 const eleSelectLevel = document.querySelector('#select-level');
-const eleBoxFlex=document.querySelector('.box-flex')
+const eleBoxFlex=document.querySelector('.box-flex');
 
-arrDate.forEach(objDate => eleBoxFlex.innerHTML += generateBox(objDate));
-
-function generateBox(obj){
-    return `
-	   <div class= "box">
-	        <i class="${obj.prefix}sharp ${obj.prefix}solid ${obj.prefix}${obj.name}" ></i>
-			<h4>${obj.name}</h4>
-	   </div>	
-	`
-}
-
-// const arrAnimal = arrDate.filter(objDate => objDate.type === 'animal');
-// const arrVegetable = arrDate.filter(objDate => objDate.type === 'vegetable');
-// const arrUser = arrDate.filter(objDate => objDate.type === 'user');
-
-// arrAnimal.forEach(objDate => eleBoxFlex.innerHTML += generateBox(objDate));
-// arrVegetable.forEach(objDate => eleBoxFlex.innerHTML += generateBox(objDate));
-// arrUser.forEach(objDate => eleBoxFlex.innerHTML += generateBox(objDate));
+renderIcons(arrDate,eleBoxFlex);
+randomizeColors(arrDate);
+populateSelect(arrDate,eleSelectLevel);
+eleSelectLevel.addEventListener('change', filterIcons);
